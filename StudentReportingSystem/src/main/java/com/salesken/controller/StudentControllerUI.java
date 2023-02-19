ac5f0da16e9aa8d64b0a0995dd26a7637794a61a
Raushan Kumar <raushan376kumar@gmail.com>
2023-02-19 06:20:06 +0530

    add

--- StudentReportingSystem/src/main/java/com/salesken/controller/StudentControllerUI.java
+++ StudentReportingSystem/src/main/java/com/salesken/controller/StudentControllerUI.java
@@ -1,10 +1,17 @@
 package com.salesken.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.salesken.exceptions.StudentException;
+import com.salesken.model.Student;
 import com.salesken.repository.StudentRepository;
+import com.salesken.services.StudentService;
 
 @Controller
 public class StudentControllerUI {
@@ -12,11 +19,18 @@ public class StudentControllerUI {
 	@Autowired
 	private StudentRepository sRepo;
 	
+	@Autowired
+	private StudentService sService;
+	
 	@GetMapping("/home")
 	public String homeLauncher() {
 		
 		return "welcome";
 	}
+	
+	
+	
+	
 	
 
 }
